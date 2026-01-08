87468df76a8c47eb622ccf9650930cc0f5dce293
LamChan <lamchan@LamChans-MacBook-Air.local>
2026-01-08 09:35:52 +0700

    xpathATC

--- src/test/java/heroku/FormAuthenticationTest.java
+++ src/test/java/heroku/FormAuthenticationTest.java
@@ -1,10 +1,14 @@
 package heroku;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
 import javax.xml.namespace.QName;
 import java.awt.desktop.OpenFilesEvent;
 
@@ -17,7 +21,7 @@ public class FormAuthenticationTest {
 //    Click on Login button
 //    And the home page is appear
     @Test
-    void tc01() {
+    void tc01() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
 //        driver.findElement(By.tagName("input")).sendKeys("tomsmith");
@@ -39,17 +43,43 @@ public class FormAuthenticationTest {
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys("tomsmith");
 
 //        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+//
+//        driver.findElement(By.cssSelector("#password")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.cssSelector("input#password")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.cssSelector("input[id=password]")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SuperSecretPassword!");
+//
+//        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.xpath("//input[contains(@name,'password')]"));
+//
+//        driver.findElement(By.cssSelector("input[type=password]")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//input[@type='password']"))
+                .sendKeys("SuperSecretPassword!");
 
-        driver.findElement(By.cssSelector("#password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.cssSelector("input#password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.cssSelector("input[id=password]")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.tagName("button")).click();
+//        driver.findElement(By.className("radius")).click();
+//        driver.findElement(By.cssSelector(".radius"));
+//        driver.findElement(By.cssSelector("[type=submit]")).click();
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(5000);
 
-        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("SuperSecretPassword!");
+        Assert.assertEquals(driver.getCurrentUrl(),"https://the-internet.herokuapp.com/secure");
+        Assert.assertTrue(driver.findElement(By.tagName("h4"))
+                .getText()
+                .contains("Welcome to the Secure Area. When you are done click logout below."));
 
-        driver.findElement(By.cssSelector("input[type=password]")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("SuperSecretPassword!");
+        driver.quit();
+
+//        <label for="username"> Username</label>
+//        //label[.='Username']
+//        //input[@id='username']/preceding-sibling::label/../input
+//        //label[.='Username']/following-sibling::input
+//        //Label[.='Username']/../input
+
+//        By usernameInput = RelativeLocator
+//                .with(By.tagName("input"))
+//                .below(By.xpath("//label[.='Username']"));
     }
 }

3a5c25143e5f97d16cce7e74785caea1985046fc
LamChan <lamchan@LamChans-MacBook-Air.local>
2026-01-14 15:57:44 +0700

    NestedFrameTest

--- src/test/java/heroku/NestedFrameTest.java
+++ src/test/java/heroku/NestedFrameTest.java
@@ -1,4 +1,32 @@
 package heroku;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class NestedFrameTest {
+    @Test
+    void tc05(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame("frame-left");
+        Assert.assertTrue(driver.findElement(By.xpath("html/body")).getText().contains("LEFT"));
+
+        driver.switchTo().parentFrame();
+        driver.switchTo().frame("frame-middle");
+        Assert.assertTrue(driver.findElement(By.id("content")).getText().contains("MIDDLE"));
+
+        driver.switchTo().parentFrame();
+        driver.switchTo().frame("frame-right");
+        Assert.assertTrue(driver.findElement(By.xpath("html/body")).getText().contains("RIGHT"));
+
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame("frame-bottom");
+        Assert.assertTrue(driver.findElement(By.xpath("html/body")).getText().contains("BOTTOM"));
+
+        driver.quit();
+    }
 }
